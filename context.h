88550a79b85b425ebdc4619b63a85532582d2e8f
Gary Guo <nbdd0121@hotmail.com>
2015-04-30 22:24:15 +0800

    Refactor code and add exception support for windows
    Now exception also works for windows x64
    support for windows x86 and cygwin/mingw has problems

--- context.h
+++ context.h
@@ -10,81 +10,78 @@ namespace coroutine {
 extern "C" {
 #endif
 
-#define NORLIT_ABI_CDECL 0
-#define NORLIT_ABI_WIN64 1
-#define NORLIT_ABI_SYSV  2
+#define NORLIT_OS_WIN 0
+#define NORLIT_OS_NIX 1
+
+#define NORLIT_ARCH_X86 0
+#define NORLIT_ARCH_X64 1
+
 #define NORLIT_COMPILER_GCC	0
 #define NORLIT_COMPILER_VS	1
 
-#if defined(_WIN32) || defined (__CYGWIN__)
-#	if defined(_WIN64) || (defined(__CYGWIN__) && !defined(__CYGWIN32__))
-#		define NORLIT_ABI_USED NORLIT_ABI_WIN64
+#define NORLIT_ABI_CDECL 0
+#define NORLIT_ABI_WIN64 1
+#define NORLIT_ABI_SYSV  2
+
+
+#if defined(_WIN32)
+#	define NORLIT_OS NORLIT_OS_WIN
+#	if defined(_WIN64)
+#		define NORLIT_ARCH NORLIT_ARCH_X64
 #	else
-#		define NORLIT_ABI_USED NORLIT_ABI_CDECL
+#		define NORLIT_ARCH NORLIT_ARCH_X86
 #	endif
-#	if defined(__CYGWIN__) || defined(__MINGW32__)
+#	if defined(__MINGW32__)
 #		define NORLIT_COMPILER NORLIT_COMPILER_GCC
 #	else
 #		define NORLIT_COMPILER NORLIT_COMPILER_VS
 #	endif
-#else
-#	if defined(__x86_64__)
-#		define NORLIT_ABI_USED NORLIT_ABI_SYSV
+#elif defined(__CYGWIN__)
+#	define NORLIT_OS NORLIT_OS_WIN
+#	if defined(__CYGWIN32__)
+#		define NORLIT_ARCH NORLIT_ARCH_X86
 #	else
-#		define NORLIT_ABI_USED NORLIT_ABI_CDECL
+#		define NORLIT_ARCH NORLIT_ARCH_X64
+#	endif
+#	define NORLIT_COMPILER NORLIT_COMPILER_GCC
+#else
+#	define NORLIT_OS NORLIT_OS_NIX
+#	if defined(__x86_64__)
+#		define NORLIT_ARCH NORLIT_ARCH_X64
+#	else
+#		define NORLIT_ARCH NORLIT_ARCH_X86
 #	endif
 #	define NORLIT_COMPILER NORLIT_COMPILER_GCC
 #endif
 
-#if NORLIT_ABI_USED == NORLIT_ABI_CDECL
-typedef struct {
-	uint32_t ebx;
-	uint32_t esp;
-	uint32_t ebp;
-	uint32_t esi;
-	uint32_t edi;
-	uint32_t eip;
-	uint32_t mxcsr;
-} context_t;
-#elif NORLIT_ABI_USED == NORLIT_ABI_WIN64
-typedef struct {
-	uint64_t rbx;
-	uint64_t rsp;
-	uint64_t rbp;
-	uint64_t rsi;
-	uint64_t rdi;
-	uint64_t r12;
-	uint64_t r13;
-	uint64_t r14;
-	uint64_t r15;
-	uint64_t rip;
-	uint64_t xmm6[2];
-	uint64_t xmm7[2];
-	uint64_t xmm8[2];
-	uint64_t xmm9[2];
-	uint64_t xmm10[2];
-	uint64_t xmm11[2];
-	uint64_t xmm12[2];
-	uint64_t xmm13[2];
-	uint64_t xmm14[2];
-	uint64_t xmm15[2];
-	uint32_t mxcsr;
-} context_t;
-#elif NORLIT_ABI_USED == NORLIT_ABI_SYSV
-typedef struct {
-	uint64_t rbx;
-	uint64_t rsp;
-	uint64_t rbp;
-	uint64_t r12;
-	uint64_t r13;
-	uint64_t r14;
-	uint64_t r15;
-	uint64_t rip;
-	uint32_t mxcsr;
-} context_t;
+#if NORLIT_OS == NORLIT_OS_WIN
+#	define NORLIT_OS_NAME win
+#elif NORLIT_OS == NORLIT_OS_NIX
+#	define NORLIT_OS_NAME nix
 #else
-#	error Unsupported ABI
+#	error Unknown OS
 #endif
+
+#if NORLIT_ARCH == NORLIT_ARCH_X86
+#	define NORLIT_ARCH_NAME x86
+#elif NORLIT_ARCH == NORLIT_ARCH_X64
+#	define NORLIT_ARCH_NAME x64
+#else
+#	error Unknown OS
+#endif
+
+#if NORLIT_COMPILER == NORLIT_COMPILER_GCC
+#	define NORLIT_COMPILER_NAME gcc
+#elif NORLIT_COMPILER == NORLIT_COMPILER_VS
+#	define NORLIT_COMPILER_NAME vs
+#else
+#	error Unknown OS
+#endif
+
+#define NORLIT_MACRO_STIRNGIFY_N(arg) #arg
+#define NORLIT_MACRO_STIRNGIFY(arg) NORLIT_MACRO_STIRNGIFY_N(arg)
+
+#include NORLIT_MACRO_STIRNGIFY(NORLIT_ARCH_NAME/NORLIT_OS_NAME/context.h)
 
 int  context_get(context_t*);
 void context_set(const context_t*);
